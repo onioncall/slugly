18c9bead971bf398436e3b38fef0ccf35485ae09
Orion Callaghan <onioncall@mac.mynetworksettings.com>
2025-03-30 23:58:14 -0500

    gated build actions

--- examples/console/main.go
+++ examples/console/main.go
@@ -7,11 +7,6 @@ import (
 )
 
 func main() {
-    // config := slugly.SlugConfig {
-    //     Speed: slugly.FastPrintSpeed,
-    //     Writer: os.Stdout,
-    // }
-
 	config := slugly.NewConsoleSlugConfig(slugly.MediumPrintSpeed, os.Stdout)
     config.SlugPrint("Hello! This is a test string... for testing.")
 }
